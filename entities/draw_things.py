13a20a6f8a86ed1c7677b6cc658c2b9ec9e85f41
nooby <113226273+betathang@users.noreply.github.com>
2025-12-23 22:50:20 +0700

    Add files via upload

--- entities/draw_things.py
+++ entities/draw_things.py
@@ -4,15 +4,16 @@ import sys
 pygame.font.init()
 
 WHITE = (255,255,255)
+GRAY = (129, 131, 132,200)
 keyboard = [
     ["Q","W","E","R","T","Y","U","I","O","P"],
     ["A","S","D","F","G","H","J","K","L"],
     "ENTER",
     ["Z","X","C","V","B","N","M"],
-    "⌫"
+    "DELETE"
 ]
 
-font = pygame.font.SysFont(None, 48) # letter
+
 
 def draw_rect(screen,color, rect, d = 0,r = 0):
     x,y,w,h = rect
@@ -20,65 +21,52 @@ def draw_rect(screen,color, rect, d = 0,r = 0):
     pygame.draw.rect(alpha_surface,color,(0,0,w,h),width = d, border_radius = r)
     screen.blit(alpha_surface, (x, y))
 
-def draw_a_character_inside_a_box(screen,text,x,y,w,h,color):
-    rect = pygame.Rect(x, y, w, h)
+def draw_a_character_inside_a_box(screen,text,rect,color,font = pygame.font.SysFont(None, 48)):
+    rect = pygame.Rect(rect)
     text_surface = font.render(text, True, WHITE)
     text_rect = text_surface.get_rect(center=rect.center)  
     draw_rect(screen, color, rect)
     screen.blit(text_surface, text_rect)
 
-def draw_kb(screen,kb_color):#kb_color = 0 =>kb_color[text], -1 => dark kb_color[text], 1=> yellow, 2=> green
+class button:
+    def __init__(self, rect, name,font = pygame.font.SysFont(None, 48)):
+        self.rect = pygame.Rect(rect)
+        self.name = name
+        self.color = GRAY
+        self.font = font
+    def draw(self,screen):
+        draw_a_character_inside_a_box(screen, self.name,self.rect,self.color,self.font)
+    def is_mouse_on(self):
+        return self.rect.collidepoint(pygame.mouse.get_pos())
 
-    rect_height = 58
-    rect_width = 44
-    rect_distance = 6
-    screen_width, screen_height = screen.get_size()
+kb_dict = {}
 
-
-    #draw the keyboard
-    #first collum
-    y =screen_height-200
-    x = screen_width/2 - 5*rect_width - 4.5*rect_distance
-    for i in range(0,10):
-        draw_a_character_inside_a_box(screen, keyboard[0][i],x,y,rect_width,rect_height,kb_color[keyboard[0][i]])
-        x += rect_width + rect_distance
-
-    #second collum
-    y+= rect_height + rect_distance/6*8
-    x = screen_width/2 - 4.5*rect_width - 4*rect_distance
-
-    for i in range(0,9):
-        draw_a_character_inside_a_box(screen, keyboard[1][i],x,y,rect_width,rect_height,kb_color[keyboard[1][i]])
-        
-        x += rect_width + rect_distance
-    y+= rect_height + rect_distance/6*8
-    x = screen_width/2 - 3.5*rect_width - 4*rect_distance-65
-
-    #thirt collum
-    #enter
-    rect = pygame.Rect(x, y, 65, rect_height)
-    x += 65 + rect_distance
-    text = keyboard[2]
-    draw_rect(screen, kb_color[text], rect)
-    text_surface = pygame.font.SysFont(None, 24).render(text, True, WHITE)
-    text_rect = text_surface.get_rect(center=rect.center)  
-    screen.blit(text_surface, text_rect)
+def create_kb(screen):
+    w,w2,h,d,d2 = 44,65,58,6,8
+    width,height = screen.get_size()
+    x = width/2-5*w-4.5*d
+    y = height-200
+    for i in keyboard[0]:
+        kb_dict[i] = button((x,y,w,h),i)
+        x+= w + d
+    y += h+d2
+    x = width/2-4.5*w-4*d
+    for i in keyboard[1]:
+        kb_dict[i] = button((x,y,w,h),i)
+        x+= w + d
+    y += h+d2
+    x = width/2-3.5*w - 4*d - w2
+    kb_dict[keyboard[2]] = button((x,y,w2,h),keyboard[2],font = pygame.font.SysFont(None, 24))
+    x+=w2+d
+    for i in keyboard[3]:
+        kb_dict[i] = button((x,y,w,h),i)
+        x+= w + d
+    kb_dict[keyboard[4]] = button((x,y,w2,h),keyboard[4],font = pygame.font.SysFont(None, 20))
 
 
-    for i in range(0,7):
-        draw_a_character_inside_a_box(screen, keyboard[3][i],x,y,rect_width,rect_height,kb_color[keyboard[3][i]])
-
-        x += rect_width + rect_distance
-    
-    #backspace rect
-    rect = pygame.Rect(x, y, 65, rect_height)
-    draw_rect(screen, kb_color["BACKSPACE"], rect)
-    x += 65 + rect_distance
-    text = keyboard[4]
-    text_surface = text_surface = pygame.font.SysFont("Segoe UI Symbol", 28).render(text, True, WHITE)
-    text_rect = text_surface.get_rect(center=rect.center)
-    text_rect.y-=2  #not true
-    screen.blit(text_surface, text_rect)
+def draw_kb(screen):#kb_color = 0 =>kb_color[text], -1 => dark kb_color[text], 1=> yellow, 2=> green
+    for i in kb_dict:
+        kb_dict[i].draw(screen)
 
 
 def draw_maingame(screen,character_box_color,character):
@@ -93,6 +81,6 @@ def draw_maingame(screen,character_box_color,character):
                 ch = ""
             else:
                 ch = character[i][j]
-            draw_a_character_inside_a_box(screen,ch,x,y,size,size,character_box_color[i][j])
+            draw_a_character_inside_a_box(screen,ch,(x,y,size,size),character_box_color[i][j])
             x+=size+distance
         y+=distance+size
